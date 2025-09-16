2b2d51868687f9f752175ba9146249ac6a1a4d98
Nikita Aleksyeyenko <aleksyeyenko.nikita@gmail.com>
2025-09-16 21:07:27 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ echo "Starting FastMCP on 0.0.0.0:${PORT_TO_USE}"
 
 # БЫЛО (неправильно): nocodb_mcp_server:app
 # СТАЛО (правильно):   nocodb_mcp_server.py:app  ← обязательно с .py
-exec fastmcp run ./nocodb_mcp_server.py:app \
+exec fastmcp run ./nocodb_mcp_server.py \
   --transport http \
   --host 0.0.0.0 \
   --port "${PORT_TO_USE}"
